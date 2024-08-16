c315db3e96e9295972f89874a77040bdf0fa3acf
Pedro Vilas Bôas <phvb20@gmail.com>
2024-08-16 20:10:55 -0300

    Remove Everything

--- jamgame/Addons/maaacks_music_controller/maaacks_music_controller.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends EditorPlugin
-
-
-func _enter_tree():
-	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_music_controller/base/scenes/Autoloads/ProjectMusicController.tscn")
-
-func _exit_tree():
-	remove_autoload_singleton("ProjectMusicController")
