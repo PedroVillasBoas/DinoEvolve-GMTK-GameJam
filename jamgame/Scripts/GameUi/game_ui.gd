b73d65857668d8627cbf9d9d248b4bd87302b1c1
Larissa Santos <lari.santos.sobrinho@gmail.com>
2024-08-17 18:25:20 -0300

    score script organization

--- jamgame/Scripts/GameUi/game_ui.gd
+++ jamgame/Scripts/GameUi/game_ui.gd
@@ -1,12 +1,14 @@
 extends Control
 
 var score = 0
-@onready var scoreLabel = $Score
+@onready var score1 = $ScoreperTime
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _process(delta):
-	score += 0.5
-	scoreLabel.text = "Score: %dm" % score
+	score1 += 0.5
+	score1.text = "ScoreperTime: %dm" % score1
+
+#falta colocar no label o score por coletáveis, não sei como colocar :(
