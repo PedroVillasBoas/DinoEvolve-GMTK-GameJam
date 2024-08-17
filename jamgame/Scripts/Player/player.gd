6340f93747e070f56839d0e0f36676ba9b77cdae
Larissa Santos <lari.santos.sobrinho@gmail.com>
2024-08-17 13:18:11 -0300

    animação do player

--- jamgame/Scripts/Player/player.gd
+++ jamgame/Scripts/Player/player.gd
@@ -1,26 +1,30 @@
 extends CharacterBody2D
 
+const SPEED = 700.0
+const JUMP_VELOCITY = -800.0
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -600.0
-
+@onready var animated_sprite: AnimatedSprite2D = $LokiFemale/AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		if animated_sprite:
+			animated_sprite.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			animated_sprite.play("movement")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor() and animated_sprite:
+			animated_sprite.stop()
+
+	if not is_on_floor() and animated_sprite and animated_sprite.animation != "jump":
+		animated_sprite.play("jump")
 
 	move_and_slide()
-	
