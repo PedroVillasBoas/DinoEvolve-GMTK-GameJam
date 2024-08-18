8c63d4e3c7d1c2af6c65b00131b32fe045dbb2f0
Pedro Vilas Bôas <phvb20@gmail.com>
2024-08-18 12:52:56 -0300

    Add Assets and Fix Bugs

--- jamgame/Scripts/GameUi/game_ui.gd
+++ jamgame/Scripts/GameUi/game_ui.gd
@@ -1,14 +1,12 @@
 extends Control
 
-var score = 0
-@onready var score1 = $ScoreperTime
+@onready var score1 = $Score
 
-func _ready() -> void:
-	pass # Replace with function body.
+var score : float = 0
 
 
 func _process(delta):
-	score1 += 0.5
-	score1.text = "ScoreperTime: %dm" % score1
+	score += 0.5
+	score1.text = "ScoreperTime: %dm" % score
 
 #falta colocar no label o score por coletáveis, não sei como colocar :(
--- jamgame/Scripts/Player/player.gd
+++ jamgame/Scripts/Player/player.gd
@@ -6,14 +6,16 @@ const JUMP_VELOCITY = -800.0
 var can_move : bool = true
 var is_chicken : bool = false
 var is_dead : bool = false
-var score = 0
+var score : int = 0
 
 signal dead
 
 func _physics_process(delta: float) -> void:
+	# Player is Jumping
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	# Player is not dead
 	if can_move:
 		handle_jump()
 		handle_movement(delta)
